bd81b08188056a690d8ba6d4bd1020de4a787843
Benjamin Daiß <benjamin.daiss@student.uibk.ac.at>
2024-05-23 18:16:24 +0200

    removed slash

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ let controlElevation = L.control.elevation({
     height: 300,
     theme: "bike-tirol",
 }).addTo(map);
-controlElevation.load("/data/etappe10.gpx");
+controlElevation.load("data/etappe10.gpx");
 
 let pulldown = document.querySelector("#pulldown");
 
